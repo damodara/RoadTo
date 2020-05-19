3ac5a52420f13b88f6f57f78eb92b2cec41b2aaf
damodara <jay108@mail.ru>
2020-05-20 01:18:26 +0300

    create template of place page

---
+++ Road/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Road
+//
+//  Created by ddas on 20.05.2020.
+//  Copyright © 2020 ddas. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+        .clipShape(Circle())
+        .overlay(Circle().stroke(Color.white, lineWidth: 4))
+        .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
--- Road/ContentView.swift
+++ Road/ContentView.swift
@@ -10,11 +10,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("""
-Hello World!
-We did it!
-with second 
-""")
+        ScrollView {
+            VStack{
+                MapView()
+                    .edgesIgnoringSafeArea(.top)
+                    .frame(height: 300)
+                CircleImage()
+                    .offset(y:-130)
+                    .padding(.bottom, -130)
+                VStack(alignment: .leading) {
+                    Text("Name")
+                        .font(.title)
+                    HStack{
+                        Text("Location")
+                            .font(.subheadline)
+                        Spacer()
+                        Text("Type place")
+                            .font(.subheadline)
+                    }
+                    Spacer()
+                        Text("""
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                Quas consectetur adipiscing elit audiam virtute ut, case utamur fuisset eam ut, iisque accommodare an eam. Reque blandit qui eu, cu vix nonumy volumus. Legendos intellegam id usu, vide oporteat vix eu, id illud principes has. Nam tempor utamur gubergren no.
+
+                Audiam virtute ut, case utamur fuisset eam ut, iisque accommodare an eam. Reque blandit qui eu, cu vix nonumy volumus. Legendos intellegam id usu, vide oporteat vix eu, id illud principes has. Nam tempor utamur gubergren no. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                Nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                """)
+                                        
+                                    
+                }
+                .padding()
+            }
+        }
         
     }
 }
